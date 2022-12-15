e54e12c2b68eb7de8ee5873b8b7d1ac3dc442104
Manes79 <mszornel@gmail.com>
2022-12-15 20:47:00 +0100

    initial commit

---
+++ src/main/java/pl/manes/currencyandgoldrates/CurrencyAndGoldRatesApplication.java
@@ -0,0 +1,13 @@
+package pl.manes.currencyandgoldrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAndGoldRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAndGoldRatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/manes/currencyandgoldrates/CurrencyAndGoldRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manes.currencyandgoldrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAndGoldRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
